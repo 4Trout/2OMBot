7e52c3c3b02fb2ea42f66ab3e3ff310e13b9915e
4Trout <56208177+4Trout@users.noreply.github.com>
2022-02-18 18:19:09 -0500

    Added wallet role features
    - Integrated MySQL database
    - Added commands:
      - $verify - does ?check and ?add
      - ?check - checks for wallet existence in the database
      - ?lookup - checks for user existence in the database
      - ?add - adds the user and wallet to the database
      - ?remove - removes the user from the databse

--- bot.py
+++ bot.py
@@ -1,21 +1,145 @@
 import config
 import random
 import asyncio
+import discord
 from discord import Game
+from discord import Color
+from discord.ext import commands
 from discord.ext.commands import Bot
+from discord.ext.commands import CommandNotFound
+
+# Coingecko setup
 from pycoingecko import CoinGeckoAPI
 cg = CoinGeckoAPI()
 
-BOT_PREFIX = ("?", "!")
+# MySQL setup
+from mysql.connector import (connection)
+cnx = connection.MySQLConnection(user=config.dbuser,password=config.dbpass,host=config.dbhost,database=config.db)
+cursor = cnx.cursor()
+
+# Global bot config
+BOT_PREFIX = ("?")
 client = Bot(command_prefix=BOT_PREFIX)
 
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        return
+    if isinstance(error, commands.MissingRole):
+        em = discord.Embed(title=f"Permission Error", description=f"You don't have permission to use this command!", color=Color.red())
+        await ctx.send(embed=em)
+        return
+    raise error
+
+# Bot commands
 @client.command()
-async def twomb(ctx):
-    await twoPrice('2omb')
+@commands.has_role(config.supportRole)
+async def verify(ctx, user: discord.Member, wallet, role:discord.Role=None):
+    res = await dbWalletCheck(wallet)
+
+    if res:
+        em = discord.Embed(title=f"Error: Already Registered!", description=f"Wallet already exists for user: <@" + res[0] + ">", color=Color.red())
+        await ctx.send(embed=em)
+    else:
+        await dbAdd(wallet, user.id)
+        if role is None:
+            em = discord.Embed(title=f"Verification Success!", description=f"Wallet has been verified, please grant user appropriate role!", color=Color.blue())
+            await ctx.send(embed=em)
+        else:
+            await user.add_roles(role)
+            em = discord.Embed(title=f"Verification Success!", description=f"Wallet has been verified and role has been automatically granted!", color=Color.green())
+            await ctx.send(embed=em)
+
 
 @client.command()
-async def thromb(ctx):
-    await threePrice('2omb')
+@commands.has_role(config.supportRole)
+async def add(ctx, user: discord.Member, wallet, role: discord.Role=None):
+    await dbAdd(wallet, user.id)
+    if role is None:
+        em = discord.Embed(title=f"Verification Success!", description=f"Wallet has been verified, please grant user appropriate role if necessary!", color=Color.blue())
+        await ctx.send(embed=em)
+    else:
+        await user.add_roles(role)
+        em = discord.Embed(title=f"Verification Success!", description=f"Wallet has been verified and role has been automatically granted!", color=Color.green())
+        await ctx.send(embed=em)
+
+@client.command()
+@commands.has_role(config.supportRole)
+async def check(ctx, addr):
+    res = await dbWalletCheck(addr)
+
+    if res:
+        em = discord.Embed(title=f"Already Registered!", description=f"Wallet already exists for user: <@" + res[0] + ">", color=Color.blue())
+        await ctx.send(embed=em)
+    else:
+        em = discord.Embed(title=f"Not Registered!", description=f"Wallet is not yet registered!", color=Color.blue())
+        await ctx.send(embed=em)
+
+@client.command()
+@commands.has_role(config.supportRole)
+async def lookup(ctx, user: discord.Member):
+    res = await dbUserCheck(user.id)
+
+    if res:
+        em = discord.Embed(title=f"Wallet Found!", description=f"User wallet is: " + res[0], color=Color.blue())
+        await ctx.send(embed=em)
+    else:
+        em = discord.Embed(title=f"Not Registered!", description=f"User has no wallet registered!", color=Color.blue())
+        await ctx.send(embed=em)
+
+@client.command()
+@commands.has_role(config.supportRole)
+async def remove(ctx, wallet):
+    await dbRemove(wallet)
+    em = discord.Embed(title=f"Wallet Removed!", description=f"Wallet was removed from verification system!", color=Color.blue())
+    await ctx.send(embed=em)
+
+
+# @client.command()
+# async def twomb(ctx):
+#     await twoPrice('2omb')
+#
+# @client.command()
+# async def thromb(ctx):
+#     await threePrice('2omb')
+
+# FUNCTIONS
+
+async def dbWalletCheck(addr):
+    checkQuery = 'SELECT discId FROM wallets WHERE address = %s'
+    checkData = (addr,)
+
+    cursor.execute(checkQuery, checkData)
+    if cursor.rowcount > 0:
+        return False
+    else:
+        return cursor.fetchone()
+
+async def dbUserCheck(discId):
+    checkQuery = 'SELECT address FROM wallets WHERE discId = %s'
+    checkData = (discId,)
+
+    cursor.execute(checkQuery, checkData)
+    if cursor.rowcount > 0:
+        return False
+    else:
+        return cursor.fetchone()
+
+async def dbAdd(addr, discId):
+    addQuery = 'REPLACE INTO wallets (address, discId) VALUES (%s, %s)'
+    addData = (addr, discId)
+
+    cursor.execute(addQuery, addData)
+    cnx.commit()
+    return
+
+async def dbRemove(addr):
+    removeQuery = 'DELETE FROM wallets WHERE address = %s'
+    removeData = (addr,)
+
+    cursor.execute(removeQuery, removeData)
+    cnx.commit()
+    return
 
 async def twoPrice(name):
     json = cg.get_price(ids=['2share', '2omb-finance', 'fantom'], vs_currencies='usd')
@@ -36,6 +160,6 @@ async def getPeg(price, ftm):
 
 async def updateStatus(title, val):
     label = title + ': ' + val
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=label))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=label))
 
 client.run(config.secret)
