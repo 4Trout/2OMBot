5a79318ad73530cdcd6d458f5bc1e20a9334b289
4Trout <56208177+4Trout@users.noreply.github.com>
2022-02-24 14:24:30 -0500

    Reworked mysql connections to fire off and close after each query to avoid timeout/keepalive issues

--- bot.py
+++ bot.py
@@ -9,10 +9,12 @@ from discord.ext.commands import Bot
 from discord.ext.commands import CommandNotFound
 from discord.utils import get
 
+# Coingecko setup
+from pycoingecko import CoinGeckoAPI
+cg = CoinGeckoAPI()
+
 # MySQL setup
 from mysql.connector import (connection)
-cnx = connection.MySQLConnection(user=config.dbuser,password=config.dbpass,host=config.dbhost,database=config.db)
-cursor = cnx.cursor()
 
 # Global bot config
 BOT_PREFIX = ("?")
@@ -95,9 +97,21 @@ async def remove(ctx, wallet):
     em = discord.Embed(title=f"Wallet Removed!", description=f"Wallet was removed from verification system!", color=Color.blue())
     await ctx.send(embed=em)
 
+
+# @client.command()
+# async def twomb(ctx):
+#     await twoPrice('2omb')
+#
+# @client.command()
+# async def thromb(ctx):
+#     await threePrice('2omb')
+
 # FUNCTIONS
 
 async def dbWalletCheck(addr):
+    cnx = connection.MySQLConnection(user=config.dbuser,password=config.dbpass,host=config.dbhost,database=config.db)
+    cursor = cnx.cursor()
+
     checkQuery = 'SELECT discId FROM wallets WHERE address = %s'
     checkData = (addr,)
 
@@ -107,7 +121,12 @@ async def dbWalletCheck(addr):
     else:
         return cursor.fetchone()
 
+    cnx.close()
+
 async def dbUserCheck(discId):
+    cnx = connection.MySQLConnection(user=config.dbuser,password=config.dbpass,host=config.dbhost,database=config.db)
+    cursor = cnx.cursor()
+
     checkQuery = 'SELECT address FROM wallets WHERE discId = %s'
     checkData = (discId,)
 
@@ -117,20 +136,51 @@ async def dbUserCheck(discId):
     else:
         return cursor.fetchone()
 
+    cnx.close()
+
 async def dbAdd(addr, discId):
+    cnx = connection.MySQLConnection(user=config.dbuser,password=config.dbpass,host=config.dbhost,database=config.db)
+    cursor = cnx.cursor()
+
     addQuery = 'REPLACE INTO wallets (address, discId) VALUES (%s, %s)'
     addData = (addr, discId)
 
     cursor.execute(addQuery, addData)
     cnx.commit()
+    cnx.close()
     return
 
 async def dbRemove(addr):
+    cnx = connection.MySQLConnection(user=config.dbuser,password=config.dbpass,host=config.dbhost,database=config.db)
+    cursor = cnx.cursor()
+
     removeQuery = 'DELETE FROM wallets WHERE address = %s'
     removeData = (addr,)
 
     cursor.execute(removeQuery, removeData)
     cnx.commit()
+    cnx.close()
     return
+
+async def twoPrice(name):
+    json = cg.get_price(ids=['2share', '2omb-finance', 'fantom'], vs_currencies='usd')
+    print(json)
+    #await client.say("{} coin price is: ${}".format(name, value))
+
+async def threePrice(name):
+    json = cg.get_price(ids=['30mb-token', '3shares', 'fantom'], vs_currencies='usd')
+    print(json)
+    #await client.say("{} coin price is: ${}".format(name, value))
+
+async def getData(coin):
+    if coin == '3omb' or coin == '2omb':
+        getPeg()
+
+async def getPeg(price, ftm):
+    return
+
+async def updateStatus(title, val):
+    label = title + ': ' + val
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=label))
 
 client.run(config.secret)
